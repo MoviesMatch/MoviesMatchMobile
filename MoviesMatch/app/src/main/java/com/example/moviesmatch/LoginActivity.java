1f4f3938946e8788ee5041403cfa256b55d01c90
med_amine_rhallab <1832012@cegeplimoilou.ca>
2021-04-07 13:10:42 -0400

    Creation de la page login completed

--- MoviesMatch/app/src/main/java/com/example/moviesmatch/LoginActivity.java
+++ MoviesMatch/app/src/main/java/com/example/moviesmatch/LoginActivity.java
@@ -3,12 +3,15 @@ package com.example.moviesmatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.moviesmatch.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
 }
