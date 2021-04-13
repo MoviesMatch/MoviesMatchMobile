92d49f2071ea5897121be204d7627ed1afdf3623
EtienneDro <70449475+EtienneDro@users.noreply.github.com>
2021-04-13 10:55:59 -0400

    Splash screen

--- MoviesMatch/app/src/main/java/com/example/moviesmatch/SplashActivity.java
+++ MoviesMatch/app/src/main/java/com/example/moviesmatch/SplashActivity.java
@@ -1,14 +1,45 @@
 package com.example.moviesmatch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
 
+    Handler handler;
+    TextView moviesMatch;
+    Animation animMoviesMatch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        moviesMatch = findViewById(R.id.moviesMatch);
+        animation();
+
+
+        //TEST
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+            }
+        },2000);
+    }
+
+    private void animation(){
+        animMoviesMatch = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.blink);
+        moviesMatch.startAnimation(animMoviesMatch);
     }
 }
