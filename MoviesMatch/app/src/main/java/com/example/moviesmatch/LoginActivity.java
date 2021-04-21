c11efe8c8e43e506c75dd22420469b2cea3006d4
med_amine_rhallab <1832012@cegeplimoilou.ca>
2021-04-21 14:13:36 -0400

    la requete pour le get du login est terminée

--- MoviesMatch/app/src/main/java/com/example/moviesmatch/LoginActivity.java
+++ MoviesMatch/app/src/main/java/com/example/moviesmatch/LoginActivity.java
@@ -3,28 +3,76 @@ package com.example.moviesmatch;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import com.example.moviesmatch.async.LoginGet;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText editTextEmail;
+    EditText editTextPsw;
+    LoginGet loginGet;
+    JSONArray jsonArray;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPsw = findViewById(R.id.editTextPsw);
+        loginGet = new LoginGet(this);
     }
 
-    public void login(View view){
-        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+
+    public void setJsonArray(JSONArray jsonArray) {
+        this.jsonArray = jsonArray;
     }
 
-    public void register(View view){
+
+    public void login(View view) {
+        if (checkLogin()) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        }else{
+            new AlertDialog.Builder(this).setTitle("Account").setMessage("Your email or password is incorrect.").show();
+        }
+    }
+
+
+    public boolean checkLogin() {
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject user = null;
+            try {
+                user = jsonArray.getJSONObject(i);
+                String userEmail = user.getString("email");
+                String userPsw = user.getString("password");
+                if (editTextEmail.getText().toString().equals(userEmail) && editTextPsw.getText().toString().equals(userPsw)) {
+                    return true;
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public void register(View view) {
         startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
     }
 
+
     @Override
-    public void onBackPressed(){
+    public void onBackPressed() {
         //Do nothing
     }
 }
--- MoviesMatch/app/src/main/java/com/example/moviesmatch/async/LoginGet.java
+++ MoviesMatch/app/src/main/java/com/example/moviesmatch/async/LoginGet.java
@@ -1,4 +1,58 @@
 package com.example.moviesmatch.async;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.moviesmatch.LoginActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.ref.WeakReference;
+
 public class LoginGet {
+
+    String url = "https://my.api.mockaroo.com/users.json?key=86d5d270";
+    private WeakReference<LoginActivity> weakReference;
+    protected LoginActivity loginActivity;
+    private RequestQueue queue;
+
+
+    public LoginGet(LoginActivity loginActivity) {
+        weakReference = new WeakReference<LoginActivity>(loginActivity);
+        this.loginActivity = weakReference.get();
+        queue = Volley.newRequestQueue(loginActivity);
+        queue.start();
+        getUsers();
+    }
+
+    public void getUsers() {
+        System.out.println("LOOK AT THIS ");
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("Users");
+                    loginActivity.setJsonArray(jsonArray);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.err.println(error);
+            }
+        });
+        queue.add(request);
+    }
+
 }
