911880b976d34bf2d5f6e2c5ade2ad68e3edf628
Med Amine Rhallab <70241115+marhallab@users.noreply.github.com>
2021-05-02 16:41:40 -0400

    Mov 82/requete login (#23)
    * Creation de la classe loginGet
    
    * la requete pour le get du login est terminée
    
    * Correction du get en post
    
    * Ajout de la validation de l'email avant la post
    
    * Changement du url pour le post vers l'api
    
    * Update l'URL
    
    * Ûpdate du code rapide
    
    * Delete LoginPost.java
    
    * Adapter le post pour le login avec la classe postRequest
    
    * Ajout de l url et du onBackPressed

--- MoviesMatch/app/src/main/java/com/example/moviesmatch/LoginActivity.java
+++ MoviesMatch/app/src/main/java/com/example/moviesmatch/LoginActivity.java
@@ -1,30 +1,94 @@
 package com.example.moviesmatch;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+import com.example.moviesmatch.async.PostRequest;
+import com.example.moviesmatch.certificate.CertificateByPass;
+import com.example.moviesmatch.interfaces.IPostActivity;
+import com.example.moviesmatch.interfaces.IPostCallback;
+import com.example.moviesmatch.validation.InputsValidation;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class LoginActivity extends AppCompatActivity implements IPostActivity {
+
+    EditText editTextEmail;
+    EditText editTextPsw;
+    PostRequest postRequest;
+    JSONObject jsonObject;
+    InputsValidation validation;
+    CertificateByPass certificat;
+
+    final static String URL = "/api/user/login";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setUp();
     }
 
-    public void login(View view){
-        startActivity(new Intent(LoginActivity.this, MainActivity.class));
-    }
 
-    public void register(View view){
-        startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+    public void login(View view) {
+
+        try {
+            jsonObject.put("usrEmail", editTextEmail.getText().toString());
+            jsonObject.put("usrPassword", editTextPsw.getText().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        if (validation.validateEmail(editTextEmail.getText().toString())) {
+            postRequest.postRequest(jsonObject, URL, new IPostCallback() {
+                @Override
+                public void onSuccess(JSONObject jsonObject) {
+                    startAct(jsonObject);
+                }
+            });
+        }
     }
 
     @Override
-    public void onBackPressed(){
-        //Do nothing
+    public void onErrorResponseAlert(int errorCode) {
+        new AlertDialog.Builder(this).setTitle("Your email or password are wrong").setMessage("Please change your email or password").show();
     }
+
+    public void setUp() {
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPsw = findViewById(R.id.editTextPsw);
+        jsonObject = new JSONObject();
+        postRequest = new PostRequest(this);
+        validation = new InputsValidation(this);
+        certificat = new CertificateByPass();
+        certificat.IngoreCertificate();
+    }
+
+
+    public void register(View view) {
+        startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+    }
+
+
+    public void startAct(JSONObject jsonObject) {
+        Intent i = new Intent(LoginActivity.this, MainActivity.class);
+        i.putExtra("CurrentUser",jsonObject.toString());
+        startActivity(i);
+    }
+    @Override
+    public void onBackPressed(){
+        
+    }
+
 }
