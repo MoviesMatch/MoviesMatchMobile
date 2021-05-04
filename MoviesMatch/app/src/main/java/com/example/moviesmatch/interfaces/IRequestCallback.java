6f527789b067b712cc9d24b1aa6c3001847a7d7c
EtienneDro <70449475+EtienneDro@users.noreply.github.com>
2021-05-04 11:09:52 -0400

    GetRequestArray (#37)

--- MoviesMatch/app/src/main/java/com/example/moviesmatch/interfaces/IRequestCallback.java
+++ MoviesMatch/app/src/main/java/com/example/moviesmatch/interfaces/IRequestCallback.java
@@ -1,7 +1,9 @@
 package com.example.moviesmatch.interfaces;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public interface IRequestCallback {
     void onSuccess(JSONObject jsonObject);
+    void onSuccess(JSONArray jsonObject);
 }
