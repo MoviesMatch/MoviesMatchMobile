6ef2d7d2d1bf3093067a26c1d444fbef5cd3d60f
EtienneDro <70449475+EtienneDro@users.noreply.github.com>
2021-05-05 11:09:01 -0400

    PostRequest accepte les reponse null

--- MoviesMatch/app/src/main/java/com/example/moviesmatch/async/PostRequest.java
+++ MoviesMatch/app/src/main/java/com/example/moviesmatch/async/PostRequest.java
@@ -2,10 +2,13 @@ package com.example.moviesmatch.async;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.moviesmatch.interfaces.IPostActivity;
@@ -13,6 +16,7 @@ import com.example.moviesmatch.interfaces.IRequestCallback;
 
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
 import java.lang.ref.WeakReference;
 
 public class PostRequest {
@@ -43,15 +47,36 @@ public class PostRequest {
             public void onErrorResponse(VolleyError error) {
                 System.out.println("Error " + error);
                 //Calls each classes onErrorResponseAlert method that implements IPostActivity to have custom error messages
-                if (activity instanceof IPostActivity){
-                    try{
+                if (activity instanceof IPostActivity) {
+                    try {
                         ((IPostActivity) activity).onPostErrorResponse(error.networkResponse.statusCode);
-                    } catch (NullPointerException e){
+                    } catch (NullPointerException e) {
                         ((IPostActivity) activity).onPostErrorResponse(0);
                     }
                 }
             }
-        });
+        }) {
+            @Override
+            protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
+                try {
+                    String json = new String(
+                            response.data,
+                            "UTF-8"
+                    );
+
+                    if (json.length() == 0) {
+                        return Response.success(
+                                null,
+                                HttpHeaderParser.parseCacheHeaders(response)
+                        );
+                    } else {
+                        return super.parseNetworkResponse(response);
+                    }
+                } catch (UnsupportedEncodingException e) {
+                    return Response.error(new ParseError(e));
+                }
+            }
+        };
         queue.add(jsonObjectRequest);
     }
 }
