17011347c4dfc4ace3f1c6a81cf667530efe78c2
EtienneDro <70449475+EtienneDro@users.noreply.github.com>
2021-05-06 18:48:54 -0400

    Binding

--- app/src/main/java/com/example/moviesmatch/layouts/activities/LoginActivity.java
+++ app/src/main/java/com/example/moviesmatch/layouts/activities/LoginActivity.java
@@ -8,9 +8,11 @@ import androidx.appcompat.app.AppCompatDelegate;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.moviesmatch.R;
+import com.example.moviesmatch.databinding.ActivityLoginBinding;
 import com.example.moviesmatch.requests.PostRequest;
 import com.example.moviesmatch.certificate.CertificateByPass;
 import com.example.moviesmatch.interfaces.IPostActivity;
@@ -21,29 +23,33 @@ import com.example.moviesmatch.validation.InputsValidation;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import pl.droidsonroids.gif.GifImageView;
+
 public class LoginActivity extends AppCompatActivity implements IPostActivity {
 
-    EditText editTextEmail;
-    EditText editTextPsw;
-    PostRequest postRequest;
-    JSONObject jsonObject;
-    InputsValidation validation;
-    CertificateByPass certificat;
+    private ActivityLoginBinding binding;
+    private EditText editTextEmail;
+    private EditText editTextPsw;
+    private PostRequest postRequest;
+    private JSONObject jsonObject;
+    private InputsValidation validation;
+    private CertificateByPass certificat;
+    private GifImageView loadingGif;
+    private Button loginButton;
 
-    final static String URL = "/api/user/login";
-
+    private final static String URL = "/api/user/login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         setUp();
     }
 
 
     public void login(View view) {
-
         try {
             jsonObject.put("usrEmail", editTextEmail.getText().toString());
             jsonObject.put("usrPassword", editTextPsw.getText().toString());
@@ -67,8 +73,8 @@ public class LoginActivity extends AppCompatActivity implements IPostActivity {
     }
 
     public void setUp() {
-        editTextEmail = findViewById(R.id.editTextEmail);
-        editTextPsw = findViewById(R.id.editTextPsw);
+        editTextEmail = binding.editTextEmail;
+        editTextPsw = binding.editTextPsw;
         jsonObject = new JSONObject();
         postRequest = new PostRequest(this);
         validation = new InputsValidation(this);
