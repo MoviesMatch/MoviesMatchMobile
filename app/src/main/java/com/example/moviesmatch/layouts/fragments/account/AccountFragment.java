4b6816741eff0f20759861e2a0c1e746863a0681
med_amine_rhallab <1832012@cegeplimoilou.ca>
2021-05-18 14:39:29 -0400

    les requetes sont faites tout et presque accomplis.

--- app/src/main/java/com/example/moviesmatch/layouts/fragments/account/AccountFragment.java
+++ app/src/main/java/com/example/moviesmatch/layouts/fragments/account/AccountFragment.java
@@ -5,27 +5,42 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.moviesmatch.R;
 import com.example.moviesmatch.databinding.FragmentAccountBinding;
+import com.example.moviesmatch.interfaces.IRequestCallback;
+import com.example.moviesmatch.requests.PutRequest;
 import com.example.moviesmatch.validation.CountryAbbreviation;
+import com.example.moviesmatch.validation.InputsValidation;
 import com.example.moviesmatch.validation.JSONManipulator;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class AccountFragment extends Fragment {
 
     private FragmentAccountBinding binding;
-    private Button buttonChangePassword;
-    private TextView textViewFullName, textViewEmail, textViewCountry;
+    private Button buttonChangePassword, buttonSave;
+    private EditText editTextFirstName, editTextLastName;
+    private TextView textViewEmail;
+    private Spinner spinnerCountry;
     private JSONManipulator jsonManipulator;
-    private JSONObject account;
+    private JSONObject account,updAccount;
     private CountryAbbreviation countryAbbreviation;
+    private PutRequest putRequest;
+    private InputsValidation inputsValidation;
+    private String URL = "/api/user/updateUser";
+    private String token ;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -36,36 +51,83 @@ public class AccountFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        refresh();
         setUp();
         fillInfos();
         goToChangePassword();
+        saveChanges();
     }
 
     private void setUp() {
-        textViewFullName = binding.textViewFullName;
+        editTextFirstName = binding.editTextFirstName;
         textViewEmail = binding.textViewEmail;
-        textViewCountry = binding.textViewCountry;
+        editTextLastName = binding.editTextLastName;
+        spinnerCountry = binding.spinnerCountry;
         buttonChangePassword = binding.buttonChangePassword;
+        buttonSave = binding.buttonSave;
+        updAccount = new JSONObject();
         countryAbbreviation = new CountryAbbreviation();
         jsonManipulator = new JSONManipulator();
         account = jsonManipulator.newJSONObject(getArguments().getString("Account"));
+        putRequest = new PutRequest((AppCompatActivity) getActivity());
+        inputsValidation = new InputsValidation(getContext());
+        token =  jsonManipulator.getString(account, "token");
     }
 
-    private void fillInfos(){
+    private void fillInfos() {
         System.out.println(account);
-        textViewFullName.setText(jsonManipulator.getJSONObjectGetString(account,"userDB","usrFirstname") + " " + jsonManipulator.getJSONObjectGetString(account,"userDB","usrLastname"));
-        textViewEmail.setText(jsonManipulator.getJSONObjectGetString(account,"userDB","usrEmail"));
-        textViewCountry.setText(countryAbbreviation.getCountryAbbreviationReverse(jsonManipulator.getJSONObjectGetString(account,"userDB","usrCountry")));
+        editTextFirstName.setText(jsonManipulator.getJSONObjectGetString(account, "userDB", "usrFirstname"));
+        editTextLastName.setText(jsonManipulator.getJSONObjectGetString(account, "userDB", "usrLastname"));
+        textViewEmail.setText(jsonManipulator.getJSONObjectGetString(account, "userDB", "usrEmail"));
+
+        // textViewCountry.setText(countryAbbreviation.getCountryAbbreviationReverse(jsonManipulator.getJSONObjectGetString(account,"userDB","usrCountry")));
     }
 
 
-    private void goToChangePassword(){
+    private void saveChanges() {
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (inputsValidation.validateName(editTextFirstName.getText().toString(), editTextLastName.getText().toString())) {
+                    try {
+                        updAccount.put("usrId", jsonManipulator.getJSONObjectGetString(account,"userDB","usrId"));
+                        updAccount.put("usrFirstname", editTextFirstName.getText().toString());
+                        updAccount.put("usrLastname", editTextLastName.getText().toString());
+                        updAccount.put("usrCountry", countryAbbreviation.getCountryAbbreviation(spinnerCountry.getSelectedItem().toString()));
+                        updateAccount();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    private void updateAccount() {
+        putRequest.putRequest(updAccount, URL, token, new IRequestCallback() {
+            @Override
+            public void onSuccess(JSONObject jsonObject) {
+                new AlertDialog.Builder(getContext()).setTitle("Success!").setMessage("Your infos has been saved.").show();
+            }
+        });
+    }
+
+    private void refresh(){
+        AccountFragment currentFragment = new AccountFragment();
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.detach(currentFragment);
+        fragmentTransaction.attach(currentFragment);
+        fragmentTransaction.commit();
+    }
+
+    private void goToChangePassword() {
         buttonChangePassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AccountPasswordFragment fragment = new AccountPasswordFragment();
                 Bundle bundle = new Bundle();
-                bundle.putString("account",account.toString());
+                bundle.putString("Account", account.toString());
                 fragment.setArguments(bundle);
                 getFragmentManager().beginTransaction().replace(R.id.frame, fragment).addToBackStack(null).commit();
             }
--- app/src/main/java/com/example/moviesmatch/layouts/fragments/account/AccountPasswordFragment.java
+++ app/src/main/java/com/example/moviesmatch/layouts/fragments/account/AccountPasswordFragment.java
@@ -2,20 +2,113 @@ package com.example.moviesmatch.layouts.fragments.account;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.moviesmatch.R;
+import com.example.moviesmatch.databinding.FragmentAccountBinding;
+import com.example.moviesmatch.databinding.FragmentAccountPasswordBinding;
+import com.example.moviesmatch.interfaces.IOnBackPressed;
+import com.example.moviesmatch.interfaces.IRequestCallback;
+import com.example.moviesmatch.layouts.activities.MainActivity;
+import com.example.moviesmatch.layouts.fragments.GroupsFragment;
+import com.example.moviesmatch.requests.PutRequest;
+import com.example.moviesmatch.validation.InputsValidation;
+import com.example.moviesmatch.validation.JSONManipulator;
+import com.example.moviesmatch.validation.PasswordsEye;
 
-public class AccountPasswordFragment extends Fragment {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class AccountPasswordFragment extends Fragment implements IOnBackPressed {
+
+    private FragmentAccountPasswordBinding binding;
+    private EditText oldPassword, newPassword, confirmNewPassword;
+    private Button buttonSavePassword;
+    private PasswordsEye passwordsEye;
+    private PutRequest putRequest;
+    private String URL = "/api/user/changePassword";
+    private JSONObject account, updAccount;
+    private String token;
+    private JSONManipulator jsonManipulator;
+    private InputsValidation inputsValidation;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account_password, container, false);
+        binding = FragmentAccountPasswordBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setUp();
+        passwordsEye();
+        savePassword();
+    }
+
+    private void passwordsEye() {
+        passwordsEye.passwordEye(oldPassword);
+        passwordsEye.passwordEye(newPassword);
+        passwordsEye.passwordEye(confirmNewPassword);
+    }
+
+    private void setUp() {
+        oldPassword = binding.editTextOldPassword;
+        newPassword = binding.editTextNewPassword;
+        confirmNewPassword = binding.editTextConfirmNewPassword;
+        buttonSavePassword = binding.buttonSavePassword;
+        passwordsEye = new PasswordsEye();
+        updAccount = new JSONObject();
+        jsonManipulator = new JSONManipulator();
+        account = jsonManipulator.newJSONObject(getArguments().getString("Account"));
+        putRequest = new PutRequest((AppCompatActivity) getActivity());
+        inputsValidation = new InputsValidation(getContext());
+        token = jsonManipulator.getString(account, "token");
+    }
+
+    private void savePassword() {
+        buttonSavePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (inputsValidation.validatePassword(newPassword.getText().toString(), confirmNewPassword.getText().toString())) {
+                    try {
+                        updAccount.put("id", jsonManipulator.getJSONObjectGetString(account, "userDB", "usrId"));
+                        updAccount.put("oldPassword", oldPassword.getText().toString());
+                        updAccount.put("newPassword", newPassword.getText().toString());
+                        System.out.println(updAccount);
+                        updateAccount();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    private void updateAccount() {
+        putRequest.putRequest(updAccount, URL, token, new IRequestCallback() {
+            @Override
+            public void onSuccess(JSONObject jsonObject) {
+                new AlertDialog.Builder(getContext()).setTitle("Success!").setMessage("Your password has been changed.").show();
+            }
+        });
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        ((MainActivity) getActivity()).replaceFrag(new AccountFragment());
     }
 }
