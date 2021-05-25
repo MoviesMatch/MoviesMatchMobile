7e6f79dd487be1c5ba5724e53674aa910058d1a9
EtienneDro <70449475+EtienneDro@users.noreply.github.com>
2021-05-25 13:50:45 -0400

    Boutton pour lien vers netflix + arrangement du code xml

--- app/src/main/java/com/example/moviesmatch/layouts/fragments/MovieInfosFragment.java
+++ app/src/main/java/com/example/moviesmatch/layouts/fragments/MovieInfosFragment.java
@@ -1,5 +1,7 @@
 package com.example.moviesmatch.layouts.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -31,11 +34,12 @@ public class MovieInfosFragment extends Fragment implements IOnBackPressed {
     private RecyclerView recyclerView;
     private GridLayoutManager layoutManager;
     private RecyclerViewAdapter recyclerViewAdapter;
-    private TextView textViewInfoTitle, textViewInfoDate, textViewInfoOverview, textViewRating, textViewRunTime, textViewUrl;
+    private TextView textViewInfoTitle, textViewInfoDate, textViewInfoOverview, textViewRating, textViewRunTime;
     private ImageView poster;
     private Calculator calculator;
     private ArrayList<String> genres;
     private String title, overview, posterUrl, releaseYear, imdbRating, runtime, url;
+    private Button buttonUrl;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -48,28 +52,10 @@ public class MovieInfosFragment extends Fragment implements IOnBackPressed {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        setUp();
+        init();
         getJsonObject();
         addInfos();
     }
-
-
-    public void setUp() {
-        genres = new ArrayList<>();
-        calculator = new Calculator();
-
-        recyclerView = binding.recyclerView;
-        layoutManager = new GridLayoutManager(getContext(), 2);
-
-        textViewInfoTitle = binding.textViewInfoTitle;
-        textViewInfoDate = binding.textViewInfoDate;
-        textViewInfoOverview = binding.textViewInfoOverview;
-        textViewRating = binding.textViewRating;
-        textViewRunTime = binding.textViewRunTime;
-        textViewUrl = binding.textViewUrl;
-        poster = binding.imageViewPoster;
-    }
-
 
     public void getJsonObject() {
         Bundle bundle = this.getArguments();
@@ -86,16 +72,41 @@ public class MovieInfosFragment extends Fragment implements IOnBackPressed {
     }
 
     public void addInfos() {
-
         textViewInfoTitle.setText(title);
         textViewInfoDate.setText(releaseYear);
         textViewInfoOverview.setText(overview);
         textViewRating.setText(calculator.rating(imdbRating));
         textViewRunTime.setText(calculator.runTime(runtime));
-        textViewUrl.setText(url);
+        buttonUrlOnClick(url);
         Picasso.get().load(posterUrl).into(poster);
-
         initRecyclerView();
+    }
+
+    private void buttonUrlOnClick(String netflixUrl){
+        buttonUrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uriUrl = Uri.parse(netflixUrl);
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
+            }
+        });
+    }
+
+    public void init() {
+        genres = new ArrayList<>();
+        calculator = new Calculator();
+
+        recyclerView = binding.recyclerView;
+        layoutManager = new GridLayoutManager(getContext(), 2);
+
+        textViewInfoTitle = binding.textViewInfoTitle;
+        textViewInfoDate = binding.textViewInfoDate;
+        textViewInfoOverview = binding.textViewOverview;
+        textViewRating = binding.textViewRating;
+        textViewRunTime = binding.textViewRunTime;
+        buttonUrl = binding.buttonUrl;
+        poster = binding.imageViewPoster;
     }
 
     public void initRecyclerView() {
@@ -105,13 +116,11 @@ public class MovieInfosFragment extends Fragment implements IOnBackPressed {
         recyclerViewAdapter.notifyDataSetChanged();
     }
 
-
     @Override
     public void onBackPressed() {
         FragmentManager fm = getFragmentManager();
         if (fm.getBackStackEntryCount() > 0) {
             fm.popBackStack();
-        } else {
         }
     }
 }
