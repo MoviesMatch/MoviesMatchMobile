59cef6f813cea1d4425ef3a6041d8b9b7ef27a73
EtienneDro <70449475+EtienneDro@users.noreply.github.com>
2021-06-02 12:57:11 -0400

    OnBackPressed dans Login et CreateAccount (#74)

--- app/src/main/java/com/example/moviesmatch/layouts/activities/CreateAccountActivity.java
+++ app/src/main/java/com/example/moviesmatch/layouts/activities/CreateAccountActivity.java
@@ -8,12 +8,16 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.moviesmatch.R;
+import com.example.moviesmatch.layouts.fragments.MovieInfosFragment;
+import com.example.moviesmatch.layouts.fragments.SwipeFragment;
 import com.example.moviesmatch.layouts.fragments.signup.SignUpNameFragment;
 import com.example.moviesmatch.layouts.fragments.signup.SignUpPasswordFragment;
 import com.example.moviesmatch.databinding.ActivityCreateAccountBinding;
 import com.example.moviesmatch.interfaces.IGetActivity;
 import com.example.moviesmatch.interfaces.IPostActivity;
 import com.example.moviesmatch.validation.OnErrorResponse;
+
+import java.util.List;
 
 public class CreateAccountActivity extends AppCompatActivity implements IPostActivity, IGetActivity {
     private ActivityCreateAccountBinding binding;
@@ -43,6 +47,22 @@ public class CreateAccountActivity extends AppCompatActivity implements IPostAct
             return true;
         }
         return false;
+    }
+
+    @Override
+    public void onBackPressed() {
+        tellFragments();
+    }
+
+    private void tellFragments() {
+        List<Fragment> fragments = getSupportFragmentManager().getFragments();
+        for (Fragment f : fragments) {
+            if (f != null && f instanceof SignUpNameFragment) {
+                ((SignUpNameFragment) f).onBackPressed();
+            } else if (f != null && f instanceof SignUpPasswordFragment) {
+                ((SignUpPasswordFragment) f).onBackPressed();
+            }
+        }
     }
 
     @Override
