c982721a9f07f190ea94731e73cdc4dfdac3a96f
Med Amine Rhallab <70241115+marhallab@users.noreply.github.com>
2021-06-09 14:07:42 -0400

    Mov 176/ajouter delete user (#85)
    * Ajout du bouton delete user
    
    * Update AccountFragment.java
    
    * Update AccountFragment.java

--- app/src/main/java/com/example/moviesmatch/layouts/fragments/account/AccountFragment.java
+++ app/src/main/java/com/example/moviesmatch/layouts/fragments/account/AccountFragment.java
@@ -1,6 +1,9 @@
 package com.example.moviesmatch.layouts.fragments.account;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,11 +21,15 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.example.moviesmatch.R;
 import com.example.moviesmatch.databinding.FragmentAccountBinding;
+import com.example.moviesmatch.interfaces.IDeleteActivity;
 import com.example.moviesmatch.interfaces.IOnBackPressed;
 import com.example.moviesmatch.interfaces.IRequestCallback;
+import com.example.moviesmatch.layouts.activities.LoginActivity;
 import com.example.moviesmatch.layouts.activities.MainActivity;
+import com.example.moviesmatch.layouts.activities.ResetPasswordActivity;
 import com.example.moviesmatch.layouts.fragments.GroupsFragment;
 import com.example.moviesmatch.models.MoviesMatchURLS;
+import com.example.moviesmatch.requests.DeleteRequest;
 import com.example.moviesmatch.requests.GetRequest;
 import com.example.moviesmatch.requests.PutRequest;
 import com.example.moviesmatch.validation.CountryAbbreviation;
@@ -32,10 +39,10 @@ import com.example.moviesmatch.validation.JSONManipulator;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class AccountFragment extends Fragment implements IOnBackPressed {
+public class AccountFragment extends Fragment implements IOnBackPressed, IDeleteActivity {
 
     private FragmentAccountBinding binding;
-    private Button buttonChangePassword, buttonSave;
+    private Button buttonChangePassword, buttonSave, buttonDeleteUser;
     private EditText editTextFirstName, editTextLastName;
     private TextView textViewEmail;
     private Spinner spinnerCountry;
@@ -44,9 +51,11 @@ public class AccountFragment extends Fragment implements IOnBackPressed {
     private CountryAbbreviation countryAbbreviation;
     private PutRequest putRequest;
     private GetRequest getRequest;
+    private DeleteRequest deleteRequest;
     private InputsValidation inputsValidation;
     private String URLUpdate = MoviesMatchURLS.updateUserURL;
     private String URLGetAccount;
+    private String URLDeleteAccount;
     private String token;
 
     @Override
@@ -62,6 +71,7 @@ public class AccountFragment extends Fragment implements IOnBackPressed {
         getAccountById();
         goToChangePassword();
         saveChanges();
+        deleteUser();
     }
 
     private void setUp() {
@@ -71,12 +81,14 @@ public class AccountFragment extends Fragment implements IOnBackPressed {
         spinnerCountry = binding.spinnerCountry;
         buttonChangePassword = binding.buttonChangePassword;
         buttonSave = binding.buttonSave;
+        buttonDeleteUser = binding.buttonDeleteUser;
         updAccount = new JSONObject();
         countryAbbreviation = new CountryAbbreviation();
         jsonManipulator = new JSONManipulator();
         account = jsonManipulator.newJSONObject(getArguments().getString("Account"));
         putRequest = new PutRequest((AppCompatActivity) getActivity());
         getRequest = new GetRequest((AppCompatActivity) getActivity());
+        deleteRequest = new DeleteRequest((AppCompatActivity) getActivity());
         inputsValidation = new InputsValidation(getContext());
         token = jsonManipulator.getString(account, "token");
     }
@@ -89,17 +101,17 @@ public class AccountFragment extends Fragment implements IOnBackPressed {
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        URLGetAccount +=  usrId;
+        URLGetAccount += usrId;
     }
 
-    private void getAccountById(){
+    private void getAccountById() {
         setAccountURL();
         getRequest.getRequest(URLGetAccount, token, new IRequestCallback() {
             @Override
             public void onSuccess(JSONObject jsonObject) {
                 editTextFirstName.setText(jsonManipulator.getString(jsonObject, "usrFirstname"));
-                editTextLastName.setText(jsonManipulator.getString(jsonObject,  "usrLastname"));
-                textViewEmail.setText(jsonManipulator.getString(jsonObject,  "usrEmail"));
+                editTextLastName.setText(jsonManipulator.getString(jsonObject, "usrLastname"));
+                textViewEmail.setText(jsonManipulator.getString(jsonObject, "usrEmail"));
             }
         });
     }
@@ -146,8 +158,65 @@ public class AccountFragment extends Fragment implements IOnBackPressed {
         });
     }
 
+    private void deleteUser() {
+        buttonDeleteUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+                builder.setTitle("Delete my account definetly ");
+                builder.setMessage("Please enter your password in order to delete your account: ");
+
+
+                // Set up the input
+                final EditText input = new EditText(getContext());
+
+                input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                builder.setView(input);
+
+
+                URLDeleteAccount = MoviesMatchURLS.deleteUserURL;
+
+
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        URLDeleteAccount += "?idUser=" + jsonManipulator.getJSONObjectGetString(account, "userDB","usrId") + "&password=" + input.getText().toString();
+                        deleteRequest.deleteRequest(URLDeleteAccount, token, new IRequestCallback() {
+                            @Override
+                            public void onSuccess(JSONObject jsonObject) {
+                                startActivity(new Intent((MainActivity)getActivity(), LoginActivity.class));
+                            }
+                        });
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+
+    }
+
     @Override
     public void onBackPressed() {
-        ((MainActivity)getActivity()).replaceFrag(new GroupsFragment());
+        ((MainActivity) getActivity()).replaceFrag(new GroupsFragment());
+    }
+
+    @Override
+    public void onDeleteErrorResponse(int errorCode) {
+        if(errorCode == 401 || errorCode == 400){
+            new AlertDialog.Builder(getContext()).setTitle("Delete failed").setMessage("Wrong password").show();
+        } else {
+            new AlertDialog.Builder(getContext()).setTitle("Error").setMessage("Make sure you are connected to an internet connection and try again").show();
+        }
     }
 }
